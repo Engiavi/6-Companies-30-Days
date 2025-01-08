7b5fb36995c75242aa429bdde31677d20dff2dfc
Engiavi <hackavi796@gmail.com>
2025-01-08 22:28:12 +0530

    Count the Number of Incremovable Subarrays I solution

---
+++ Microsoft/Count the Number of Incremovable Subarrays I.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int incremovableSubarrayCount(vector<int>& nums) {
+        int ans = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                bool ok = true;
+                int lst = -1;
+                
+                for (int k = 0; k < n; k++) {
+                    if (k >= i && k <= j) {
+                    } else {
+                        ok &= (lst < nums[k]);
+                        lst = nums[k];
+                    }
+                }
+                ans += ok;
+            }
+        }
+        return ans;
+    }
+};
+
+// time complexity:- O(n^3)
+// space complexity:- O(1)
